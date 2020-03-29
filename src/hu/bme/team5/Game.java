516c2907b50219bed486bd64fa35cefe8f8c567b
Petra Pristyák <petra_pristyak@sch.bme.hu>
2020-03-29 22:02:44 +0200

    Skeleton

--- src/hu/bme/team5/Game.java
+++ src/hu/bme/team5/Game.java
@@ -6,13 +6,15 @@ public class Game {
     private ArrayList<Map> maps;
     private Map currentMap;
 
-    public Game() {
-    }
+    public Game() { }
 
     public void loadMap(Map m) {
     }
 
     public void startGame() {
+        System.out.println(">currentMap.init();");
+        currentMap.init();
+        System.out.println("<currentMap.init();");
     }
 
     public void saveMap() {
