3f490fc03a02ad116a849de72d63ff7b045e1c8a
Geri <Geri@DESKTOP-O23DBKM>
2020-03-30 13:37:28 +0200

    Drowning option added

--- src/hu/bme/team5/Menu.java
+++ src/hu/bme/team5/Menu.java
@@ -9,15 +9,15 @@ public class Menu {
         System.out.println("Valasszon esemenyt:");
         System.out.println("1. Inicializacio");
         System.out.println("2. Jatekos lep");
-        System.out.println("3. Jatekos havat takarit el egy jegtablarol");
-        System.out.println("4. Jatekos kias egy targyat egy jegtablabol");
-        System.out.println("5. Jatekos felvesz egy targyat");
-        System.out.println("6. Jatekos eldob egy targyat");
-        System.out.println("7. Jatekos eszik");
-        System.out.println("8. Sarkkutato megvizsgal egy jegtablat");
-        System.out.println("9. Eszkimo iglut epit");
-        System.out.println("10. Jatekos kimenti vizbe esett tarsat");
-        System.out.println("11. Jatekos osszeszereli a jelzopisztolyt");
+        System.out.println("4. Jatekos havat takarit el egy jegtablarol");
+        System.out.println("5. Jatekos kias egy targyat egy jegtablabol");
+        System.out.println("6. Jatekos felvesz egy targyat");
+        System.out.println("7. Jatekos eldob egy targyat");
+        System.out.println("8. Jatekos eszik");
+        System.out.println("9. Sarkkutato megvizsgal egy jegtablat");
+        System.out.println("10. Eszkimo iglut epit");
+        System.out.println("11. Jatekos kimenti vizbe esett tarsat");
+        System.out.println("12. Jatekos osszeszereli a jelzopisztolyt");
         Scanner sc = new Scanner(System.in);
         int event = 0;
         
@@ -46,18 +46,23 @@ public class Menu {
 
                     game.currentMap.characters.get(0).drown();
 
-
-
                     System.out.println("<nextField.stepOn(game.currentMap.characters.get(0))");
                 } break;
-                case 4: break;
-                case 5: break;
-                case 6: break;
+                case 4: {
+
+                } break;
+                case 5: {
+
+                } break;
+                case 6: {
+
+                } break;
                 case 7: break;
                 case 8: break;
                 case 9: break;
                 case 10: break;
                 case 11: break;
+                case 12: break;
                 default: {
                     sc.close();
                     runningGame = false;
