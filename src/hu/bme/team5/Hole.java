f38c252f8b2e4814357ecb73b1cdd4599575f6f6
benjamin <benjamin@localhost.localdomain>
2020-03-30 13:24:24 +0200

    lol#

--- src/hu/bme/team5/Hole.java
+++ src/hu/bme/team5/Hole.java
@@ -7,17 +7,16 @@ public class Hole extends Field {
 
     @Override
     public void stepOn(Character c) {
-        System.out.println("    >setCharacter(c)");
+        System.out.println(">setCharacter(c)");
         setCharacter(c);
-        System.out.println("    <setCharacter(c)");
+        System.out.println("<setCharacter(c)");
 
-        System.out.println("    >setSnowSize(0)");
+        System.out.println(">setSnowSize(0)");
         setSnowSize(0);
-        System.out.println("    <setSnowSize(0)");
+        System.out.println("<setSnowSize(0)");
 
-        System.out.println("    >characters.get(characters.size()-1).drown()");
+        System.out.println(">characters.get(characters.size()-1).drown()");
         characters.get(characters.size()-1).drown();
-
-        System.out.println("    <characters.get(characters.size()-1).drown()");
+        System.out.println("<characters.get(characters.size()-1).drown()");
     }
 }
