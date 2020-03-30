8fc9a614559a60d7eba2dd79043ae75dca57171d
Akos Gergely <geakos01@hotmail.hu>
2020-03-30 12:38:18 +0200

    try3

--- src/hu/bme/team5/Program.java
+++ src/hu/bme/team5/Program.java
@@ -3,7 +3,7 @@ package hu.bme.team5;
 public class Program {
 
     public static void main(String[] args) {
-    	 System.out.print("Szia Benji!");
+    	 System.out.print("Szia Petra, Szia Benji!");
         Menu menu = new Menu();
     }
 }
