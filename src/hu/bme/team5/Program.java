16a9edb011ebc9cd6000b3045ef93a8c336a111f
Petra Pristyák <petra_pristyak@sch.bme.hu>
2020-03-30 23:12:15 +0200

    Eskimo builds igloo(9)

--- src/hu/bme/team5/Program.java
+++ src/hu/bme/team5/Program.java
@@ -4,7 +4,6 @@ public class
 Program {
 
     public static void main(String[] args) {
-    	 System.out.print("Szia Petra, Szia Benji!, Szia Aki, asdsad");
         Menu menu = new Menu();
     }
 }
