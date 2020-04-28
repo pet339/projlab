c052c3582f87f251898c8188ade91cd01e35650a
Petra Pristyák <petra_pristyak@sch.bme.hu>
2020-04-28 10:40:02 +0200

    Test

--- src/hu/bme/team5/Test.java
+++ src/hu/bme/team5/Test.java
@@ -51,39 +51,31 @@ public class Test {
                 }
                 break;
                 case "shovel": {
-                    playerShovelSnowTest(line, raf.readLine());
                 }
                 break;
                 case "dig": {
-                    playerStepsOnHoleTest(line, raf.readLine());
                 }
                 break;
                 case "give": {
-                    playerDigsItemTest(line, raf.readLine());
                 }
                 break;
                 case "eat": {
 
-                    playerTradesItemTest(line, raf.readLine());
                 }
                 break;
                 case "inspect": {
-                    playerEatsTest(line, raf.readLine());
 
                 }
                 break;
                 case "igloo": {
-                    playerExploreTest(line, raf.readLine());
 
                 }
                 break;
                 case "save": {
-                    playerEskimoBuildsIglooTest(line, raf.readLine());
 
                 }
                 break;
                 case "aasemble": {
-                    playerSavesPlayerTest(line, raf.readLine());
 
                 }
                 break;
@@ -101,57 +93,5 @@ public class Test {
             System.out.println("Success");
         else
             System.out.println("Fail");
-    }
-
-    /*
-     * @param input: String, output: String
-     * @return void
-     */
-    public void initTest(String input, String output) {
-
-    }
-
-    /*
-     * @param input: String, output: String
-     * @return void
-     */
-    public void playerStepTest(String input, String output) {
-
-    }
-
-    public boolean playerShovelSnowTest(String input, String output) {
-        return true;
-    }
-
-    public boolean playerStepsOnHoleTest(String input, String output) {
-        return true;
-    }
-
-    public boolean playerDigsItemTest(String input, String output) {
-        return true;
-    }
-
-    public boolean playerTradesItemTest(String input, String output) {
-        return true;
-    }
-
-    public boolean playerEatsTest(String input, String output) {
-        return true;
-    }
-
-    public boolean playerExploreTest(String input, String output) {
-        return true;
-    }
-
-    public boolean playerEskimoBuildsIglooTest(String input, String output) {
-        return true;
-    }
-
-    public boolean playerSavesPlayerTest(String input, String output) {
-        return true;
-    }
-
-    public boolean playerAssemblesTest(String input, String output) {
-        return true;
     }
 }
