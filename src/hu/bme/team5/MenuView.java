16c585c63e36d4dd56e91623e0de0d7175025fd1
Petra Pristyák <petra_pristyak@sch.bme.hu>
2020-05-12 22:18:11 +0200

    MenuView- Choose characters

--- src/hu/bme/team5/MenuView.java
+++ src/hu/bme/team5/MenuView.java
@@ -6,24 +6,67 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 
+import static hu.bme.team5.Menu.game;
+
 public class MenuView extends JFrame {
 
     private PlayPanel activePanel = null;
-
+    private JButton startGame = new JButton("Start playing");
     private JFrame frame = new JFrame("Menu");
+    private JFrame frame2 = new JFrame("Choose characters");
     private JPanel jp = new JPanel();
     private JButton start = new JButton();
     private JButton exit = new JButton();
 
     public MenuView() {
         try {
-            initWindow();
+            initFrame();
+            initFrame2();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    void initWindow() throws IOException {
+    void initFrame2() {
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        startGame.addActionListener(new ActionMenuButtonListener());
+        startGame.setText("Start playing");
+
+        JPanel jPanel = new JPanel();
+        JLabel label = new JLabel(("Number of characters: "));
+        JComboBox comboBox = new JComboBox();
+        for (int i = 0; i < 5; i++)
+            comboBox.addItem(i + 1);
+        jPanel.add(label);
+        jPanel.add(comboBox);
+
+        int numberOfCharacters = (int) comboBox.getSelectedItem();
+
+        JPanel jPanel1 = new JPanel();
+        JLabel label1 = new JLabel("Character 1:");
+        JComboBox comboBox1 = new JComboBox();
+        comboBox1.addItem("Explorer");
+        comboBox1.addItem("Eskimo");
+        JLabel label1_2 = new JLabel("Name:");
+        JTextArea textArea1 = new JTextArea("Dora");
+        textArea1.setPreferredSize(new Dimension(200, 20));
+        jPanel1.add(label1);
+        jPanel1.add(comboBox1);
+        jPanel1.add(label1_2);
+        jPanel1.add(textArea1);
+
+
+        panel.add(jPanel);
+        panel.add(jPanel1);
+
+        frame2.add(startGame, BorderLayout.SOUTH);
+        frame2.add(panel, BorderLayout.CENTER);
+        frame2.setSize(700, 500);
+        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    void initFrame() throws IOException {
 
         start.addActionListener(new MenuView.ActionMenuButtonListener());
         exit.addActionListener(new MenuView.ActionMenuButtonListener());
@@ -56,16 +99,20 @@ public class MenuView extends JFrame {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            Map m = new Map(new Game());
-            m.init();
             if (e.getActionCommand().equals(start.getActionCommand())) {
-                if (activePanel == null)
-                    activePanel = new PlayPanel(m);
+
+                frame2.setVisible(true);
+                frame.setVisible(false);
             }
             if (e.getActionCommand().equals(exit.getActionCommand()))
                 System.exit(0);
+
+            if (e.getActionCommand().equals(startGame.getActionCommand())) {
+                if (activePanel == null) {
+                    activePanel = new PlayPanel(game.currentMap);
+                    frame2.setVisible(false);
+                }
+            }
         }
-
-
     }
 }
