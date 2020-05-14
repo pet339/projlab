32420670fb1ada14055bf59fc6549ff6822ef1d1
Petra Pristyák <petra_pristyak@sch.bme.hu>
2020-05-14 11:33:19 +0200

    MenuView- Choose characters

--- src/hu/bme/team5/MenuView.java
+++ src/hu/bme/team5/MenuView.java
@@ -41,7 +41,7 @@ public class MenuView extends JFrame {
         jPanel.add(label);
         jPanel.add(comboBox);
 
-        int numberOfCharacters = (int) comboBox.getSelectedItem();
+        //int numberOfCharacters = (int) comboBox.getSelectedItem();
 
         JPanel jPanel1 = new JPanel();
         JLabel label1 = new JLabel("Character 1:");
@@ -57,8 +57,65 @@ public class MenuView extends JFrame {
         jPanel1.add(textArea1);
 
 
+        JPanel jPanel2 = new JPanel();
+        JLabel label2 = new JLabel("Character 2:");
+        JComboBox comboBox2 = new JComboBox();
+        comboBox2.addItem("Explorer");
+        comboBox2.addItem("Eskimo");
+        JLabel label2_2 = new JLabel("Name:");
+        JTextArea textArea2 = new JTextArea("Indiana Jones");
+        textArea2.setPreferredSize(new Dimension(200, 20));
+        jPanel2.add(label2);
+        jPanel2.add(comboBox2);
+        jPanel2.add(label2_2);
+        jPanel2.add(textArea2);
+
+        JPanel jPanel3 = new JPanel();
+        JLabel label3 = new JLabel("Character 3:");
+        JComboBox comboBox3 = new JComboBox();
+        comboBox3.addItem("Explorer");
+        comboBox3.addItem("Eskimo");
+        JLabel label3_2 = new JLabel("Name:");
+        JTextArea textArea3 = new JTextArea("Christopher Colombus");
+        textArea3.setPreferredSize(new Dimension(200, 20));
+        jPanel3.add(label3);
+        jPanel3.add(comboBox3);
+        jPanel3.add(label3_2);
+        jPanel3.add(textArea3);
+
+        JPanel jPanel4 = new JPanel();
+        JLabel label4 = new JLabel("Character 4:");
+        JComboBox comboBox4 = new JComboBox();
+        comboBox4.addItem("Explorer");
+        comboBox4.addItem("Eskimo");
+        JLabel label4_2 = new JLabel("Name:");
+        JTextArea textArea4 = new JTextArea("Ragnar Lothbrok");
+        textArea4.setPreferredSize(new Dimension(200, 20));
+        jPanel4.add(label4);
+        jPanel4.add(comboBox4);
+        jPanel4.add(label4_2);
+        jPanel4.add(textArea4);
+
+        JPanel jPanel5 = new JPanel();
+        JLabel label5 = new JLabel("Character 5:");
+        JComboBox comboBox5 = new JComboBox();
+        comboBox5.addItem("Explorer");
+        comboBox5.addItem("Eskimo");
+        JLabel label5_2 = new JLabel("Name:");
+        JTextArea textArea5 = new JTextArea("Eric The Red");
+        textArea5.setPreferredSize(new Dimension(200, 20));
+        jPanel5.add(label5);
+        jPanel5.add(comboBox5);
+        jPanel5.add(label5_2);
+        jPanel5.add(textArea5);
+
+
         panel.add(jPanel);
         panel.add(jPanel1);
+        panel.add(jPanel2);
+        panel.add(jPanel3);
+        panel.add(jPanel4);
+        panel.add(jPanel5);
 
         frame2.add(startGame, BorderLayout.SOUTH);
         frame2.add(panel, BorderLayout.CENTER);
